8ff83a3417ac09490ff11f7c2c9ea8abdc040860
Roman Gakh <Roman-Gakh@yandex.ua>
2016-07-14 15:11:21 +0300

    second commit

---
+++ app/src/main/java/com/svitroslin/flowersdelivery/MainActivity.java
@@ -0,0 +1,13 @@
+package com.svitroslin.flowersdelivery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
